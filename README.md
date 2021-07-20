b842336e75b4d698f34119b6cbc954092c264aef
James Dobbs <dobbs282@gmail.com>
2021-07-20 17:52:51 -0400

    added vars for what will be access

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # HW-3-Code-Quiz
 
+## Story
+
+```
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+```
+
 ## Accepted Criteria
 
 - [] Start button
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,33 @@
 // What is Global?
+var highScore = document.querySelector("a");
+var timer = document.querySelector("#timer");
 
+var question = document.querySelector("#question");
+
+var answerOne = document.querySelector(".answer1");
+var answerTwo = document.querySelector(".answer2");
+var answerThree = document.querySelector(".answer3");
+var answerFour = document.querySelector(".answer4");
+
+var solution = document.querySelector("#showAnswer");
+
+var start = document.querySelector("#start");
+var submit = document.querySelector("#submit");
+var resart = document.querySelector("#reset");
+
+console.log((answerFour.textContent = "Hello World"));
+
+// // // // var questions = [
+// // // //     {
+// // // //         question: "how does rain fall",
+// // // //         choices: [
+// // // //             "upwards",
+// // // //             "downwards",
+// // // //             "sideways"
+// // // //         ],
+// // // //         answer: "downwards"
+// // // //     }
+// // // // ]
 // Hide RESET / SUBMIT / Correct/Wrong
 
 // startGame
@@ -7,18 +35,23 @@
 // // start timer
 // // Show QUESTION
 // // Show ANSWERS
+
 // // Choice correct answer Green
 // // // Add points
 // // // Show CORRECT! for 1 second
 // // // if more Next quetion
 // // // else show score
+
 // // Choice wrong answer Red
 // // // Minus time
 // // // Show WRONG! for 1 second
 // // // if more Next quetion
 // // // else show score
-// // // // Score 2pts a quetion + timeRemaining
+
+// // // // Score 2pts a quetion + timeRemaining // // // //
 
 // StartGame
 
 // startTimer
+
+// // if more Next quetion
