3677cf68aeab0c111707ace5084908336f1ba4a8
James Dobbs <dobbs282@gmail.com>
2021-07-20 09:16:05 -0400

    add java pseudocoding with classes/ids

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+// What is Global?
+
+// Hide RESET / SUBMIT / Correct/Wrong
+
+// startGame
+// // listen 'click' START
+// // start timer
+// // Show QUESTION
+// // Show ANSWERS
+// // Choice correct answer Green
+// // // Add points
+// // // Show CORRECT! for 1 second
+// // // if more Next quetion
+// // // else show score
+// // Choice wrong answer Red
+// // // Minus time
+// // // Show WRONG! for 1 second
+// // // if more Next quetion
+// // // else show score
+// // // // Score 2pts a quetion + timeRemaining
+
+// StartGame
+
+// startTimer
